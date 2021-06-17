32e6d8d44bc02c0f0f5da09443162349e794de00
Michael J Baumli <michaeljbaumli@gmail.com>
2021-06-16 20:41:03 -0500

    Completed Part 2

--- 608-ch17-part2.py
+++ 608-ch17-part2.py
@@ -9,3 +9,39 @@
 import sqlite3
 
 connection = sqlite3.connect('books.db')
+
+import pandas as pd
+
+pd.options.display.max_columns = 10
+
+results = pd.read_sql('SELECT * FROM authors', connection, index_col=['id'])
+
+print(results)
+
+results = pd.read_sql('SELECT * FROM titles',connection)
+
+print(results)
+
+df = pd.read_sql('SELECT * FROM author_ISBN',connection)
+
+df.head()
+
+results = pd.read_sql("""SELECT id,first, last FROM authors WHERE last LIKE 'Q%'""",connection, index_col=['id'])
+
+print(results)
+
+results = pd.read_sql('SELECT title FROM titles ORDER BY title ASC',connection)
+
+print(results)
+
+results = pd.read_sql("""SELECT id,first,last FROM authors ORDER BY last, first""",connection, index_col=['id'])
+
+print(results)
+
+results = pd.read_sql("""SELECT id,first,last FROM authors ORDER BY last DESC, first ASC""",connection, index_col=['id'])
+
+print(results)
+
+results = pd.read_sql("""SELECT isbn,title,edition,copyright FROM titles WHERE title LIKE '%How to Program' ORDER BY title""",connection)
+
+print(results)
