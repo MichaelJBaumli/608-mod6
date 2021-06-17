4534f3834f70e1ac4688aabfb00e9a4a2e71a48d
Michael J Baumli <michaeljbaumli@gmail.com>
2021-06-16 20:01:21 -0500

    Starting part 2  (SQL Goodness)

---
+++ 608-ch17-part2.py
@@ -0,0 +1,11 @@
+#Program Name: 608-ch17-part2.py
+#Assignment Module 6
+#Class 44680 Block 44599 Section 01
+#Michael Baumli
+#Date: 20210616
+
+
+#Import Section
+import sqlite3
+
+connection = sqlite3.connect('books.db')
